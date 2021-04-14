0b270bd6887db176c428739b1827cc284696a40d
silansubhra <subhrajyoti1903@gmail.com>
2021-04-15 01:20:55 +0530

    adding some form component method for future use

--- to-do/src/App.js
+++ to-do/src/App.js
@@ -10,12 +10,55 @@ import React from "react";
 import TodoItem from "./todoItem";
 import todosData from "./todosData";
 
-function App() {
-  const todoItems = todosData.map((item) => (
-    <TodoItem key={item.id} item={item} />
-  ));
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      todos: todosData,
+    };
+    this.handleChange = this.handleChange.bind(this);
+  }
 
-  return <div className="todo-list">{todoItems}</div>;
+  handleChange(id) {
+    console.log(id);
+    let oldTodo = this.state.todos;
+    let newTodo = oldTodo.map((todo) => {
+      if (todo.id == id) {
+        if (todo.completed) todo.completed = false;
+        else todo.completed = true;
+      }
+      return todo;
+    });
+    this.setState({ todos: newTodo });
+    // Update state so that the item with the given id flips `completed` from false to true (or vise versa)
+    // Remember not to modify prevState directly, but instead to return a new version of state with the change you want included in that update. (Think how you might use the `.map` method to do this)
+  }
+
+  handleChangeForForms(event) {
+    const { name, value, type, checked } = event.target;
+    type === "checkbox"
+      ? this.setState({ [name]: checked })
+      : this.setState({ [name]: value });
+  }
+
+  render() {
+    const todoItems = this.state.todos.map((item) => (
+      <TodoItem key={item.id} item={item} handleChange={this.handleChange} />
+    ));
+
+    // <select
+    //                 value={this.state.favColor}
+    //                 onChange={this.handleChange}
+    //                 name="favColor"
+    //             >
+    //                 <option value="blue">Blue</option>
+    //                 <option value="green">Green</option>
+    //                 <option value="red">Red</option>
+    //                 <option value="orange">Orange</option>
+    //                 <option value="yellow">Yellow</option>
+    //             </select>
+
+    return <div className="todo-list">{todoItems}</div>;
+  }
 }
-
 export default App;
